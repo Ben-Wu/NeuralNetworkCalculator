990056d2eec820a11d1319271aaadb24619db223
Ben Wu <bw96412@hotmail.com>
2018-01-09 23:13:43 -0500

    generator for data

--- nn_calc/main.py
+++ nn_calc/main.py
@@ -12,7 +12,9 @@ def create_model():
     model.add(Dense(64))
     model.add(Dense(1))
 
-    model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
+    model.compile(optimizer='sgd',
+                  loss='mean_squared_error',
+                  metrics=['accuracy'])
 
     return model
 
@@ -26,6 +28,19 @@ def generate_data(size, min, max, seed=None):
     y = np.array([e[0] + e[1] for e in x])
 
     return x, y
+
+
+def data_generator(size, low, high, batch_size=10, seed=None):
+    """Generate random arrays to use as data"""
+    seed = seed or random.randint(0, 123123321)
+
+    while True:
+        np.random.seed(seed)
+        for i in range(0, size, batch_size):
+            x = np.random.randint(low=low, high=high,
+                                  size=(min(batch_size, size - i), 2))
+            y = np.array([e[0] + e[1] for e in x])
+            yield x, y
 
 
 if __name__ == '__main__':
